8bc1e1aec1092729b939e7118fb11ded15c2333b
snehotosh <snehotosh.banerjee@oracle.com>
2016-09-12 13:06:13 +0530

    Add files via upload

---
+++ auth.py
@@ -0,0 +1,11 @@
+'''
+THIS CODE contains all access token
+'''
+class TwitterAuth:	
+	# The consumer key and secret will be generated for you after
+	consumer_key="........."
+	consumer_secret="........."
+
+	# Create an access token under the the "Your access token" section
+	access_token="........"
+	access_token_secret="........."
